55219890bc11c33d4c28c9442589ff05545f7057
LoaiAkram <lolyakram11@gmail.com>
2021-05-11 01:47:44 +0200

    fixed force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(config('app.force_https')) {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }

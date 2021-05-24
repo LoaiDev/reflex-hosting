3ffceb4663a120b3eeec9e0d1ae72808dc1ba217
LoaiAkram <lolyakram11@gmail.com>
2021-05-24 22:51:42 +0200

    added ptero users

---
+++ app/Listeners/CreatePterodactylUser.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Listeners;
+
+class CreatePterodactylUser
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle($event)
+    {
+
+    }
+}
